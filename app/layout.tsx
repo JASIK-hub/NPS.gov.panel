6b829d6449ac93f576043e19ca65a731e5f0cb78
Jasulan <zhasik7002@gmail.com>
2026-05-18 18:37:10 +0500

    chore:metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: "nps.gov — Национальная система опросов",
   description: "Официальный портал для участия в государственных опросах",
+  verification: {
+    google: "prJUqSxGTUyDGwD-3EVjybXHkNwhYqHkDH_QHuLTCrc", 
+  },
 };
 
 export default function RootLayout({
